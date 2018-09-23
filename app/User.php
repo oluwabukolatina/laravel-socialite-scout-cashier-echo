635c130778e5e51d634f2f9c96f108cf36293958
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-09-23 18:29:59 +0100

    setup for cashier

--- app/User.php
+++ app/User.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Scout\Searchable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
     use Notifiable;
     use Searchable;
+    use Billable;
     /**
      * The attributes that are mass assignable.
      *
