15b4122f234d08f816191bd1796ea357e68caf77
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-09-23 09:04:54 +0100

    socialite method

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Mockery\CountValidator\Exception;
+use App\SocialAccount;
 
 class SocialAccountController extends Controller
 {
@@ -19,14 +20,57 @@ class SocialAccountController extends Controller
     public function handleProviderCallback(){
 
         try{
+            //contains user info
             $user = Socialite::driver($provider)->user();
         } catch(Exception $e){
             //use flash message here
             return redirect('/login');
         }
 
+        //all is well
         //user model for the user
-        //extract
+        //extract to another method
         $authUser = $this->findOCreateUser($user, $provider);
+
+        Auth::login($authUser, true);
+        return redirect('/home');
+
     }
+
+    public function findOrCreateUser($socialUser, $provider){
+
+        //if thyve logged in before
+        $account = SocialAccount::where('provider_name', $provider)
+                        ->where('provider_id', $socialUser->getId())
+                        ->first();
+
+        if($account){
+
+            return $account->user;
+
+        } else {
+
+            $user = User::where('email', $socialUser->getRmail())->first();
+
+            if(! $user){
+                $user = User::create([
+                    'email' => $socialUser->getEmail(),
+                    'name' => $socialUser->getName
+                ]);
+
+            }
+
+            //r/ship from account model
+            $user->accounts()->create([
+                'provider_name' => $provider,
+                'provider_id' => $socialUser->getId()
+
+                ]);
+
+            return $user;
+
+        }
+
+    }
+
 }
